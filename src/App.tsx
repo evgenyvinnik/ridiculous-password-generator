e3568416db286998b9a02c53907245d4fd98e10c
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-03-22 23:00:50 -0700

    cleaning up basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+import Snackbar from "@mui/material/Snackbar";
+import Box from "@mui/material/Box";
+import { AppBar, Toolbar } from "@mui/material";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function App() {
+  const [command, setCommand] = useState(
+    "Click on the land to place your first Dragonball"
+  );
+  const [message, setMessage] = useState("");
+
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const generatePassword = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Button variant="outlined">Outlined</Button>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {command}
+          </Typography>
+
+          <Button color="inherit" onClick={generatePassword}>
+            Optimize placement
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "#cfe8fc",
+          height: "80%",
+        }}
+      >
+        Test
+      </Box>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        message={message}
+      />
     </div>
   );
 }
